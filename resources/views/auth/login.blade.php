ec4170cf14cbbd3fc49305a7b0c965fc923daf96
dh.atha <athadhaiffathin@gmail.com>
2024-07-30 08:20:38 +0700

    fix auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,4 +44,10 @@
             </x-primary-button>
         </div>
     </form>
+
+    <div class="mt-4 text-center">
+        <a href="{{ route('register') }}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+            {{ __('Don\'t have an account? Register here') }}
+        </a>
+    </div>
 </x-guest-layout>
